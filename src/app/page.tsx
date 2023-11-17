c6c83c405b77f75ac130c1a497176ae0795c6c22
miralistyacahya <18221116@std.stei.itb.ac.id>
2023-11-17 16:01:52 +0700

    refactor: pindah folder + ubah config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import DeployButton from '../components/DeployButton'
 import AuthButton from '../components/AuthButton'
-import { createClient } from '@/utils/supabase/server'
-import ConnectSupabaseSteps from '@/components/ConnectSupabaseSteps'
-import SignUpUserSteps from '@/components/SignUpUserSteps'
-import Header from '@/components/Header'
+import { createClient } from './../utils/supabase/server'
+import ConnectSupabaseSteps from './../components/ConnectSupabaseSteps'
+import SignUpUserSteps from './../components/SignUpUserSteps'
+import Header from "./../components/Header"
 import { cookies } from 'next/headers'
 
 export default async function Index() {
@@ -25,10 +25,10 @@ export default async function Index() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+        {/* <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <DeployButton />
           {isSupabaseConnected && <AuthButton />}
-        </div>
+        </div> */}
       </nav>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
