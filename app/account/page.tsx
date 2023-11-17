5d38147c2ea5ac584d4f9d4f7e6d1b14f3f840f0
raniakiranaa <16521042@std.stei.itb.ac.id>
2023-11-17 14:25:59 +0700

    connect to supabase

---
+++ app/account/page.tsx
@@ -0,0 +1,10 @@
+import { createClient } from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
+
+export default async function Account() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore);
+  const { data: account } = await supabase.from("account").select();
+
+  return <pre>{JSON.stringify(account, null, 2)}</pre>
+}
