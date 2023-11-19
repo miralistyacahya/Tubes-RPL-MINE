5cf18ed5072f5126d787dd90ac8da94b27fc834a
raniakiranaa <16521042@std.stei.itb.ac.id>
2023-11-19 23:52:52 +0700

    test: test table component

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,44 @@
 import SearchBar from '@/src/components/SearchBar'
-import Table from '../../components/Table'
+import Table, { TableColumn } from '../../components/Table'
 import Pagination from '@/src/components/Pagination'
+import { createClient } from '../../utils/supabase/server';
+import { cookies } from 'next/headers';
+import ActionButton from '@/src/components/ActionButton';
+
+const columns: TableColumn[] = [
+    { label: 'username', dataKey: 'username', width: '1/4', align: 'left' },
+    { label: 'password', dataKey: 'password', width: '1/4', align: 'left' },
+    { label: 'role', dataKey: 'role', width: '1/4', align: 'center' },
+    { label: 'aksi', dataKey: 'aksi', width: '1/4', align: 'center' },
+];
 
 export default async function app() {
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore);
+    const { data: accounts, error } = await supabase.from('account').select();
+
+    console.log('Accounts:', accounts);
+    console.log('Error:', error);
+
+    if (error) {
+      // Handle the error
+      console.error('Error fetching data:', error.message);
+      return <div>Error fetching data</div>;
+    }
+  
+    const data = accounts.map((account) => ({
+      username: account.username || 'N/A',
+      password: account.password || 'N/A',
+      role: account.role || 'N/A',
+      aksi: <ActionButton />
+    }));
+
     return (
         <div className='mx-16'>
             <h1 className="heading bold-20 mt-4">Daftar Produk</h1>
             <div className="mt-4 bg-white shadow-md sm:rounded-lg">
                 <SearchBar />
-                <Table />
+                <Table columns={columns} data={data}/>
                 {/* <Pagination /> */}
             </div>
         </div>
