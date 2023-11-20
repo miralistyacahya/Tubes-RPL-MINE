0e11613c8d7dc7043e2f69b908c3423b3ce9e2a5
johanchristiansen <18221088@std.stei.itb.ac.id>
2023-11-20 22:30:21 +0700

    feat: action button neutral karena pake daisyui

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -6,10 +6,10 @@ import deleteButton from "../../public/icons/delete button.svg"
 export default function ActionButton(){
     return(
         <div className="p-2 flex justify-center space-x-4">
-            <button className="btn btn-info btn-sm">
+            <button className="btn-neutral btn-info btn-sm">
                 <Image src={editButton} alt="edit"/>
             </button>
-            <button className="btn btn-error btn-sm">
+            <button className="btn-neutral btn-error btn-sm">
                 <Image src={deleteButton} alt="delete"/>
             </button>
         </div>
