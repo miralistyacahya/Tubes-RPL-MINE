268d136e8a9397318912501460e14a96b3fbb869
audarcy <16521298@std.stei.itb.ac.id>
2023-11-22 08:25:32 +0700

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,9 @@
-import Link from 'next/link'
 import { headers, cookies } from 'next/headers'
 import { createClient } from '../../utils/supabase/server'
 import { redirect } from 'next/navigation'
+import './pagelogin.css'
+import icon from '../../../public/icons/online 1.svg'
+import Image from 'next/image'
 
 export default function Login({
   searchParams,
@@ -11,13 +13,13 @@ export default function Login({
   const signIn = async (formData: FormData) => {
     'use server'
 
-    const email = formData.get('email') as string
+    const username = formData.get('username') as string
     const password = formData.get('password') as string
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
     const { error } = await supabase.auth.signInWithPassword({
-      email,
+      username,
       password,
     })
 
@@ -32,13 +34,13 @@ export default function Login({
     'use server'
 
     const origin = headers().get('origin')
-    const email = formData.get('email') as string
+    const username = formData.get('username') as string
     const password = formData.get('password') as string
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
     const { error } = await supabase.auth.signUp({
-      email,
+      username,
       password,
       options: {
         emailRedirectTo: `${origin}/auth/callback`,
@@ -53,66 +55,56 @@ export default function Login({
   }
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{' '}
-        Back
-      </Link>
 
-      <form
-        className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action={signIn}
-      >
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
-          Sign In
+    <div className="main-container">
+      {/* Header */}
+      <div className="header-container">
+        {/* Logo */}
+        <div className="logo-container">
+          <Image src= {icon} alt="Logo" />
+          <div className="regular-30 heading">MINE</div>
+        </div>
+      </div>
+
+      {/* Main Content */}
+      <div className="main-content">
+        {/* Background Box */}
+        <div className="background-container"></div>
+
+        {/* Welcome Text */}
+        <div className="welcome-text">Welcome</div>
+
+        {/* Form */}
+        <div className="form-container">
+          <div className="form-divider"></div>
+          <div className="form-divider"></div>
+          {/* Form input fields */}
+            <input
+              className="form-input"
+              name="username"
+              placeholder="Username"
+              required
+            />
+            <input
+              className="form-input"
+              name="password"
+              placeholder="Password"
+              required
+            />
+        </div>
+
+        {/* Image */}
+        <div className="image-container">
+          <Image src= {icon} alt="Logo" />
+        </div>
+
+        {/* Login Button */}
+        <button className="login-button" onClick={signIn}>
+          LOGIN
         </button>
-        <button
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-        >
-          Sign Up
-        </button>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
-      </form>
+      </div>
     </div>
   )
 }
+
+
