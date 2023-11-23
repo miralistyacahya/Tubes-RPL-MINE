793fae133463fb768e035179a4601c4814cec153
johanchristiansen <18221088@std.stei.itb.ac.id>
2023-11-23 13:10:15 +0700

    feat: komponen hapus data

--- src/app/account/hapusAkses.tsx
+++ src/app/account/hapusAkses.tsx
@@ -10,6 +10,7 @@ import hapus from '../../../public/icons/delete button.svg';
 import hapusmerah from '../../../public/icons/hapusmerah.svg';
 import batal from '../../../public/icons/batal.svg';
 import Image from 'next/image';
+import HapusData from '@/src/components/HapusData';
 
 interface HapusAksesProps {
   username: string;
@@ -17,80 +18,19 @@ interface HapusAksesProps {
 }
 
 function HapusAkses({ username, onDelete }: HapusAksesProps) {
-  const [modal, setModal] = useState(false);
-  const [isMutating, setIsMutating] = useState(false);
-  const supabase = createClient();
-
-  async function handleDelete(e: SyntheticEvent) {
-    e.preventDefault();
-    setIsMutating(true);
-
-    try {
-      const { error } = await supabase
-        .from('account')
-        .delete()
-        .eq('username', username); // Delete the specific user based on the username
-
-      if (error) {
-        throw error;
-      }
-
-      console.log('Data berhasil dihapus');
-      onDelete(username); // Notify parent component about the deletion
-      setModal(false);
-    } catch (error) {
-      console.error('Terjadi kesalahan:', error);
-    } finally {
-      setIsMutating(false);
-    }
-  }
-
-  function handleChange() {
-    setModal(!modal);
-  }
-
   return (
-    <div className="p-2 flex justify-center space-x-4">
-      <button className="btn-neutral btn-danger btn-sm" onClick={handleChange}>
-        <Image src={hapus} alt="hapus" />
-      </button>
-      <input type="checkbox" checked={modal} onChange={handleChange} className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box">
-          <div className="modal-header">
-            <div className="modal-header flex justify-end">
-              <button className="close-button" onClick={handleChange}>
-                <Image src={tutup} alt="hapus" />
-              </button>
-            </div>
-            <div className='flex justify-left mb-4 pb-10'>
-            <h3 style={{ fontSize: '28px', color: '#295F9A' }} className="font-bold text-lg mb-4">
-              Hapus Akses
-            </h3></div>
-            <div className='flex justify-center pb-10' style={{ whiteSpace: 'pre-wrap', lineHeight: '1.5' }}>
-            <p style={{ fontSize: '24px', color: '#000000', wordWrap: 'break-word' }}>
-              Apakah Anda yakin ingin menghapus data akses {username}?
-            </p>
-            </div>
-              <p>Jika sudah dikonfirmasi, tindakan ini tidak dapat dikembalikan</p>
-            <div className="modal-action flex justify-center">
-            <button className="close-button" onClick={handleChange}>
-                <Image src={batal} alt="hapus" />
-                </button>
-              {!isMutating ? (
-                <button type="button" className="btn-danger" onClick={handleDelete}>
-                  <Image src={hapusmerah} alt="hapus" />
-                </button>
-              ) : (
-                <button type="button" className="btn loading">
-                  Menghapus...
-                </button>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <HapusData
+      data={username}
+      onDelete={onDelete}
+      renderInfo={(username) => (
+        <>
+          <p style={{ fontSize: '24px', color: '#000000', wordWrap: 'break-word' }}>
+            Apakah Anda yakin ingin menghapus data akses {username}?
+          </p>
+        </>
+      )}
+      modalTitle="Hapus Akses" // Ganti dengan judul yang sesuai
+    />
   );
 }
 
