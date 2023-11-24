44b2143090dcd693179d01d9ee9e5ab871f4c702
raniakiranaa <16521042@std.stei.itb.ac.id>
2023-11-24 20:58:57 +0700

    refactor: change placement file popup notification

--- src/components/cart/AddButton.tsx
+++ src/components/cart/AddButton.tsx
@@ -2,38 +2,38 @@ import React from "react";
 import Image from "next/image";
 import addButton from "@/public/icons/add button.svg";
 import addHoverButton from "@/public/icons/add button hover.svg";
-import PopupNotification from "./PopupNotification";
+import PopupNotification from "../PopupNotification";
 
 interface AddedButtonProps {
-    onButtonClick: () => void;
+  onButtonClick: () => void;
 }
 
 const AddedButton: React.FC<AddedButtonProps> = ({ onButtonClick }) => {
-    const [isClicked, setIsClicked] = React.useState(false);
+  const [isClicked, setIsClicked] = React.useState(false);
 
-    return (
-        <div className="group inline-block relative">
-            <button className="transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-0">
-                <Image src={addButton} alt="add" />
-            </button>
-            <button
-                className="transition-opacity duration-300 ease-in-out opacity-0 group-hover:opacity-100 absolute top-0 left-0"
-                onClick={() => {
-                    onButtonClick();
-                    setIsClicked(true);
-                }}
-            >
-                <Image src={addHoverButton} alt="add hover" />
-            </button>
+  return (
+    <div className="group inline-block relative">
+      <button className="transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-0">
+        <Image src={addButton} alt="add" />
+      </button>
+      <button
+        className="transition-opacity duration-300 ease-in-out opacity-0 group-hover:opacity-100 absolute top-0 left-0"
+        onClick={() => {
+          onButtonClick();
+          setIsClicked(true);
+        }}
+      >
+        <Image src={addHoverButton} alt="add hover" />
+      </button>
 
-            <PopupNotification
-                message={"Produk ditambahkan ke keranjang"}
-                color={"green"}
-                isClicked={isClicked}
-                onClicked={(onClicked) => setIsClicked(onClicked)}
-            />
-        </div>
-    );
+      <PopupNotification
+        message={"Produk ditambahkan ke keranjang"}
+        color={"green"}
+        isClicked={isClicked}
+        onClicked={(onClicked) => setIsClicked(onClicked)}
+      />
+    </div>
+  );
 };
 
 export default AddedButton;
