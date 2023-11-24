b138f32690515bc85e1bbf4af19dd19aff64f1be
johanchristiansen <18221088@std.stei.itb.ac.id>
2023-11-24 13:51:51 +0700

    feat: fix tambahdatadropdown

--- src/components/TambahDataDropdown.tsx
+++ src/components/TambahDataDropdown.tsx
@@ -4,112 +4,155 @@ import { createClient } from "@/src/utils/supabase/client";
 import Image from "next/image";
 import tutup from "../../public/icons/tutup.svg"
 import simpan from "../../public/icons/simpan.svg"
+import Button from "./Button";
 
 interface TambahDataDropdownProps {
   tableName: string;
+  formTitle: string[];
   columns: string[];
   label: string; // Menambah prop label
+  icon?: string;
+  colToBeValidate?: string;
+  dropdownCol?: string;
+  dropdownVal?: string[];
+  dropdownValId?: string[];
 }
 
-function TambahDataDropdown({ tableName, columns, label }: TambahDataDropdownProps) {
-  const [data, setData] = useState<Record<string, string>>({});
-  const [modal, setModal] = useState(false);
-  const [isMutating, setIsMutating] = useState(false);
-  const router = useRouter();
-  const supabase = createClient();
+// ... (other imports)
 
-  const handleChange = () => {
-    setModal(!modal);
-  };
+function TambahDataDropdown({tableName, formTitle, columns, label, icon, colToBeValidate, dropdownCol, dropdownVal, dropdownValId }: TambahDataDropdownProps) {
+    const [data, setData] = useState<Record<string, string>>({});
+    const [modal, setModal] = useState(false);
+    const [isMutating, setIsMutating] = useState(false);
+    const router = useRouter();
+    const supabase = createClient();
+  
+    const handleChange = () => {
+      setModal(!modal);
+    };
+  
+    const handleSubmit = async (e: SyntheticEvent) => {
+      e.preventDefault();
+      setIsMutating(true);
+  
+      try {
+        if (colToBeValidate) {
+          const validationQuery = supabase.from(tableName).select(colToBeValidate).eq(colToBeValidate, data[colToBeValidate]);
+      
+          const { data: validationData, error: validationError } = await validationQuery;
+      
+          if (validationError) {
+            throw validationError;
+          }
+      
+          if (validationData && validationData.length > 0) {
+            // Data exists in the database, perform your validation logic here
+            console.error('Validation failed. Data already exists in the database.');
+            return;
+          }
+        }
+  
 
-  const handleSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    setIsMutating(true);
-
-    try {
-      const { data: responseData, error } = await supabase
-        .from(tableName)
-        .upsert([data]);
-
-      if (error) {
-        throw error;
+        const { data: responseData, error } = await supabase
+          .from(tableName)
+          .upsert([data]);
+  
+        if (error) {
+          throw error;
+        }
+  
+        console.log(`${label} berhasil ditambahkan:`, responseData);
+  
+        // Setelah berhasil menambahkan data, reset form, close the modal, and do not refresh the page
+        setData({});
+        setModal(false);
+        window.location.reload()
+      } catch (error) {
+        console.error(`Terjadi kesalahan saat menambah ${label}:`, error);
+      } finally {
+        setIsMutating(false);
       }
-
-      console.log(`${label} berhasil ditambahkan:`, responseData);
-
-      // Setelah berhasil menambahkan data, reset form dan refresh halaman
-      setData({});
-      setModal(false);
-      window.location.reload()
-    } catch (error) {
-      console.error(`Terjadi kesalahan saat menambah ${label}:`, error);
-    } finally {
-      setIsMutating(false);
-    }
-  };
-
-return (
-    <div className="mb-4">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleChange}>
-        + Tambah {label}
-      </button>
-      <input type="checkbox" checked={modal} onChange={handleChange} className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box">
-          <div className="modal-header">
-            <div className="modal-header flex justify-end">
-              <button className="close-button " onClick={handleChange}>
-                <Image src={tutup} alt="edit" />
-              </button>
-            </div>
-            <h3 style={{ fontSize: '28px', color: '#295F9A' }} className="font-bold text-lg mb-4">
-              Tambah {label}
-            </h3>
-            <form onSubmit={handleSubmit}>
-              {columns.map((column) => (
-                <div key={column} className="form-control">
-                  <label className="label font-bold">{column === "role" ? "Role" : column}</label>
-                  {column === "role" ? (
-                    <select
-                      value={data[column] || ""}
-                      onChange={(e) => setData({ ...data, [column]: e.target.value })}
-                      className="input w-full input-bordered"
-                    >
-                      <option value="" disabled hidden>
-                        Pilih Role
-                      </option>
-                      <option value="admin">admin</option>
-                      <option value="inventaris">inventaris</option>
-                      <option value="kasir">kasir</option>
-                    </select>
-                  ) : (
-                    <input
-                      type="text"
-                      value={data[column] || ""}
-                      onChange={(e) => setData({ ...data, [column]: e.target.value })}
-                      className="input w-full input-bordered"
-                      placeholder={column}
+    };
+  
+    return (
+      <div>
+        {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleChange}>
+          + Tambah {label}
+        </button> */}
+        <Button
+          type="button"
+          title={`Tambah ${label}`}
+          icon={icon}
+          round="rounded-lg"
+          variant="btn_blue"
+          size="semibold-14"
+          onButtonClick={handleChange}
+      />
+        <input type="checkbox" checked={modal} onChange={handleChange} className="modal-toggle" />
+        <div className={`modal ${modal ? 'open' : ''}`}>
+          <div className="modal-box bg-white">
+            <div className="modal-header">
+              <div className="modal-header flex justify-end">
+                <button className="close-button " onClick={handleChange}>
+                  <Image src={tutup} alt="edit" />
+                </button>
+              </div>
+              <h3 style={{ fontSize: '28px', color: '#295F9A' }} className="font-bold text-lg mb-4 px-2">
+                Tambah {label}
+              </h3>
+              <form onSubmit={handleSubmit}>
+                {columns.map((column, index) => (
+                  <div key={column} className="form-control px-2">
+                    <label className="label font-semibold text-black">{formTitle[index]}</label>
+                    {column === dropdownCol ? (
+                      <select
+                        value={data[column] || ""}
+                        onChange={(e) => setData({ ...data, [column]: e.target.value })}
+                        className="input w-full focus:ring-2 ring-blue-500 input-bordered bg-white font-regular text-zinc-400"
+                      >
+                        <option value="" disabled hidden>
+                          Pilih {formTitle[index]}
+                        </option>
+                        {dropdownVal?.map((option, index) => (
+                          <option key={option} value={tableName === 'product' ? dropdownValId?.[index] : option}>
+                            {option}
+                          </option>
+                        ))}
+                      </select>
+                    ) : (
+                      <input
+                        type="text"
+                        value={data[column] || ""}
+                        onChange={(e) => setData({ ...data, [column]: e.target.value })}
+                        className="input w-full focus:ring-2 ring-blue-500 input-bordered bg-white font-regular text-zinc-500"
+                        placeholder={formTitle[index].toLowerCase() + "..."}
+                      />
+                    )}
+                  </div>
+                ))}
+                <div className="modal-action flex justify-center px-28">
+                  {!isMutating ? (
+                    <Button 
+                      type= "submit"
+                      title='Simpan'
+                      round='rounded-lg'
+                      variant='btn_blue'
+                      size='semibold-16'
+                      full={true}
                     />
+                  ) : (
+                    <button type="button" className="btn loading">
+                      Menyimpan...
+                    </button>
                   )}
                 </div>
-              ))}
-              <div className="modal-action flex justify-center">
-                {!isMutating ? (
-                  <button type="submit" className="btn-primary">
-                    <Image src={simpan} alt="edit" />
-                  </button>
-                ) : (
-                  <button type="button" className="btn loading">
-                    Menyimpan...
-                  </button>
-                )}
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default TambahDataDropdown;
+    );
+  }
+  
+  export default TambahDataDropdown;
+  
