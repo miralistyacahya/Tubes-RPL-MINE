8751a437c2c1c0fbd82dcf8715436526dedd9cb2
Laurensia Audrey <18221114@std.stei.itb.ac.id>
2023-11-25 13:05:33 +0700

    feat: fix register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,43 +7,48 @@ import Image from 'next/image'
 import Navbar from '@/src/components/Navbar';
 import { NAV_ADMIN, NAV_INVENTARIS, NAV_KASIR, NAV_PUBLIC } from '@/src/constants';
 
+const isAdmin = false //role === "admin"
+const isKasir = false
+const isInventaris = false
 
-export default function Login({
+
+export default function Register({
   searchParams,
 }: {
   searchParams: { message: string }
 }) {
-  const signIn = async (formData: FormData) => {
+  const signIn = async (account: FormData) => {
     'use server'
 
-    const username = formData.get('username') as string
-    const password = formData.get('password') as string
+    const email = account.get('email') as string
+    const password = account.get('password') as string
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
     const { error } = await supabase.auth.signInWithPassword({
-      username,
+      email,
       password,
     })
 
     if (error) {
-      return redirect('/login?message=Could not authenticate user')
+      return redirect('/login?message=Gagal Mengautentikasi Pengguna')
     }
 
-    return redirect('/')
+    return redirect('/account')
   }
 
-  const signUp = async (formData: FormData) => {
+  const signUp = async (account: FormData) => {
     'use server'
 
     const origin = headers().get('origin')
-    const username = formData.get('username') as string
-    const password = formData.get('password') as string
+    const username = account.get('username') as string
+    const email = account.get('email') as string
+    const password = account.get('password') as string
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
     const { error } = await supabase.auth.signUp({
-      username,
+      email,
       password,
       options: {
         emailRedirectTo: `${origin}/auth/callback`,
@@ -54,12 +59,8 @@ export default function Login({
       return redirect('/login?message=Could not authenticate user')
     }
 
-    return redirect('/login?message=Check email to continue sign in process')
+    return redirect('login')
   }
-
-  const isAdmin = false //role === "admin"
-  const isKasir = false
-  const isInventaris = false
 
   return (
     <div>
@@ -75,34 +76,32 @@ export default function Login({
               <Image src={icon} alt="Logo" className="h-25 w-25" />
             </div>
             {/* Welcome Text */}
-            <h1 className="flex justify-center bold-32 heading">Register</h1>
+            <h1 className="flex justify-center bold-32 heading">Welcome</h1>
 
           <form
             className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-            action={signIn}
+            action={signUp}
           >
             <label className="medium-16 heading" htmlFor="username">
               Username
             </label>
             <input
               className="rounded-md px-4 py-2 bg-inherit border mb-3"
-              type="text"
               name="username"
               placeholder="Masukkan username anda"
               required
             />
-            <label className="medium-16 heading" htmlFor="password">
-              Password
+            <label className="medium-16 heading" htmlFor="email">
+              Email
             </label>
             <input
               className="rounded-md px-4 py-2 bg-inherit border mb-3"
-              type="password"
-              name="password"
-              placeholder="••••••••"
+              name="email"
+              placeholder="Masukkan email anda"
               required
             />
-            <label className="medium-16 heading" htmlFor="password">
-              Konfirmasi Password
+            <label className="medium-16 heading"  htmlFor="password">
+              Password
             </label>
             <input
               className="rounded-md px-4 py-2 bg-inherit border mb-6"
@@ -111,13 +110,13 @@ export default function Login({
               placeholder="••••••••"
               required
             />
-            <button className="btn_blue rounded-md px-4 py-2 semibold-16 mb-3">
+            <button formAction={signUp} className="btn_blue rounded-md px-4 py-2 semibold-16 mb-3">
               Daftar
             </button>
 
             <p className="flexCenter regular-14">
               Sudah terdaftar ? 
-              <Link href="login" className="text-blue-500 hover:text-blue-600 px-1">
+              <Link href='login' className="text-blue-500 hover:text-blue-600 px-1">
                 Masuk ke akunmu
               </Link>
             </p>
@@ -131,7 +130,5 @@ export default function Login({
         </div>
       </div>
     </div>  
-
   )
 }
-
