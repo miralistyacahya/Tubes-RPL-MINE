847e89cb5b0eecc9139aa0307a7f524ddc69a759
Laurensia Audrey <18221114@std.stei.itb.ac.id>
2023-11-27 01:35:00 +0700

    fix: bug authbutton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-// "use client"
 import DeployButton from '../components/DeployButton'
-import AuthButton from '../components/AuthButton'
 import { createClient } from './../utils/supabase/server'
 import ConnectSupabaseSteps from './../components/ConnectSupabaseSteps'
 import SignUpUserSteps from './../components/SignUpUserSteps'
@@ -28,7 +26,7 @@ export default async function Index() {
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
+          {isSupabaseConnected}
         </div>
       </nav>
 
