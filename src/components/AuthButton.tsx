9c014bbcda2630688be2b97a081fb2fb36e7183c
Laurensia  Audrey <18221114@std.stei.itb.ac.id>
2023-11-27 05:18:04 +0000

    feat: add register validation

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,35 +1,10 @@
-//import { createClient } from './../utils/supabase/server'
-//import { createClient } from '../utils/supabase/client'
 import {createClientComponentClient} from "@supabase/auth-helpers-nextjs"
-import Link from 'next/link'
-import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { useState } from "react"
 
 export default function AuthButton() {
-  //const cookieStore = cookies()
-  //const supabase = createClient(cookieStore)
   const supabase = createClientComponentClient()
-  //const {
-   // data: { user },
-  //} = await supabase.auth.getUser()
-  //console.log("tess userr", user);
-  // const userNow = async () => {
-  //   const {data: {user}, error} = await supabase.auth.getUser()
-  //   if(error){
-  //     console.log("get user err", error);
-  //   }
-  //   if(user){
-  //     setEmailUser(user?.email)
-  //   }
-    
-  // }
 
   const signOut = async () => {
-    // 'use server'
-
-    //const cookieStore = cookies()
-    //const supabase = createClient(cookieStore)
     const {error} = await supabase.auth.signOut()
     if(error){
       console.log("logout err", error);
@@ -47,47 +22,3 @@ export default function AuthButton() {
     </div>
   ) 
 }
-// py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover
-
-
-
-// import { createClient } from './../utils/supabase/server'
-// import Link from 'next/link'
-// import { cookies } from 'next/headers'
-// import { redirect } from 'next/navigation'
-
-// export default async function AuthButton() {
-//   const cookieStore = cookies()
-//   const supabase = createClient(cookieStore)
-
-//   const {
-//     data: { user },
-//   } = await supabase.auth.getUser()
-
-//   const signOut = async () => {
-//     'use server'
-
-//     const cookieStore = cookies()
-//     const supabase = createClient(cookieStore)
-//     await supabase.auth.signOut()
-//     return redirect('/login')
-//   }
-
-//   return user ? (
-//     <div className="flex items-center gap-4">
-//       Hey, {user.email}!
-//       <form action={signOut}>
-//         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-//           Logout
-//         </button>
-//       </form>
-//     </div>
-//   ) : (
-//     <Link
-//       href="/login"
-//       className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-//     >
-//       Login
-//     </Link>
-//   )
-// }
